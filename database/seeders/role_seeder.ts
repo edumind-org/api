8e65c8bd9389f656ea69872ebd6749d4ce6b876f
Ilyes Hernandez <hdz.ilyes@gmail.com>
2025-05-10 00:39:45 +0200

    feat(seeder): create a role seeder who create basic roles (student, teacher, administrator, and parent)

---
+++ database/seeders/role_seeder.ts
@@ -0,0 +1,13 @@
+import Role from '#models/role'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await Role.createMany([
+      { name: 'Administrator', slug: 'admin', description: 'The administrator role can perform any action.' },
+      { name: 'Student', slug: 'student', description: 'The student role can only perform actions related to their studies.' },
+      { name: 'Teacher', slug: 'teacher', description: 'The teacher role can perform actions related to teaching.' },
+      { name: 'Parent', slug: 'parent', description: 'The parent role can perform actions related to their children.' },
+    ])
+  }
+}
