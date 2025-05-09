0496074a33fc27ac4febefefbd6ed0033c134e63
Ilyes Hernandez <hdz.ilyes@gmail.com>
2025-05-10 00:59:36 +0200

    fix(auth): remove unique validation in login route

--- app/validators/auth/login.ts
+++ app/validators/auth/login.ts
@@ -2,10 +2,7 @@ import vine from '@vinejs/vine'
 
 export const LoginValidator = vine.compile(
     vine.object({
-        identifier: vine.string().trim().maxLength(255).unique({
-            table: 'users',
-            column: 'identifier'
-        }),
+        identifier: vine.string().trim().maxLength(255),
         password: vine.string().trim().minLength(8).maxLength(255),
     })
 )
