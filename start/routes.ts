f24374db3f3ca4d8834120a2ced6a146fdf33961
Ilyes Hernandez <hdz.ilyes@gmail.com>
2025-05-10 01:37:40 +0200

    feat(users): setup base for users routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
+const UsersController = () => import('#controllers/users_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
 
 router.group(() => {
   router.post('/login', [LoginController, 'login'])
 }).prefix('auth')
+router.resource('users', UsersController).apiOnly().use("*", [middleware.auth(), middleware.can('manage-users')])
